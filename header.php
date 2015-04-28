70092667e465cae0b7731002f24b7e06cbb5f991
Mengtian Zhao <mz234@cornell.edu>
2015-04-27 23:47:03 -0400

    move search to header.php

--- header.php
+++ header.php
@@ -1,24 +1,28 @@
-<link href='css/headercss.css' rel='stylesheet' type='text/css'>
 <header>
     <div class="fix">
-    <nav id="navigation" class="site-navigation" role="navigation">
-        <ul class="menu">
-            <li><img src="img/logo.png" alt="" id="logo" align="middle"></li>
-            <li class="menu-item"><a href="#">About Us</a></li>
-            <li class="menu-item"><a href="#">Teams</a>
-                <div class="dropdown">
-                <ul>
-            <li class="sub-menu"><a href="#">Team 1</a></li>
-            <li class="sub-menu"><a href="#">Team 2</a></li>
-            <li class="sub-menu"><a href="#">Team 3</a></li>
+        <nav id="navigation" class="site-navigation" role="navigation">
+            <ul class="menu">
+                <li><img src="img/logo.png" alt="" id="logo" align="middle"></li>
+                <li class="menu-item"><a href="#">About Us</a></li>
+                <li class="menu-item"><a href="#">Teams</a>
+                    <div class="dropdown">
+                        <ul>
+                            <li class="sub-menu"><a href="#">Team 1</a></li>
+                            <li class="sub-menu"><a href="#">Team 2</a></li>
+                            <li class="sub-menu"><a href="#">Team 3</a></li>
 
-        </ul></div></li>
+                        </ul></div></li>
 
-            <li class="menu-item"><a href="events.php">Events</a></li>
-            <li class="menu-item"><a href="contact.php">Contact</a></li>
-            <li class="menu-item"><a href="#">Admin</a></li>
+                <li class="menu-item"><a href="events.php">Events</a></li>
+                <li class="menu-item"><a href="contact.php">Contact</a></li>
+                <li class="menu-item"><a href="#">Admin</a></li>
+                <li class="menu-item"><form id="search" method="post" action="header.php">
+                        <input type="search" name="filter" results="5" value="" />
+                        <input type="submit" value="Search" />
+                    </form>
+                </li>
+            </ul>
 
-    </ul>
         </nav>
     </div>
     <h1></h1>
--- index.php
+++ index.php
@@ -104,35 +104,7 @@
         logout();
     }
     ?>
-    <header>
-        <div class="fix">
-            <nav id="navigation" class="site-navigation" role="navigation">
-                <ul class="menu">
-                    <li><img src="img/logo.png" alt="" id="logo" align="middle"></li>
-                    <li class="menu-item"><a href="#">About Us</a></li>
-                    <li class="menu-item"><a href="#">Teams</a>
-                        <div class="dropdown">
-                            <ul>
-                                <li class="sub-menu"><a href="#">Team 1</a></li>
-                                <li class="sub-menu"><a href="#">Team 2</a></li>
-                                <li class="sub-menu"><a href="#">Team 3</a></li>
-
-                            </ul></div></li>
-
-                    <li class="menu-item"><a href="events.php">Events</a></li>
-                    <li class="menu-item"><a href="contact.php">Contact</a></li>
-                    <li class="menu-item"><a href="#">Admin</a></li>
-
-                </ul>
-                <form id="search" method="post" action="index.php">
-                    <input type="search" name="filter" results="5" value="" />
-                    <input type="submit" value="Search" />
-                </form>
-            </nav>
-        </div>
-        <h1></h1>
-
-    </header>
+    <?php include 'header.php'?>
     <div class="content-main-body">
     <div class="content-main">
 
