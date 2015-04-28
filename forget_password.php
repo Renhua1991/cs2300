ae64d761240b9513119dbf74a2ccd05232f2324d
Renhua1991 <wongrh1991@gmail.com>
2015-04-28 15:24:37 -0400

    forget password

--- forget_password.php
+++ forget_password.php
@@ -8,118 +8,15 @@
 <html>
 <head>
   <title></title>
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="../Downloads/finalproject/css/style.css">
   <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.2.min.js"></script>
 </head>
 <body>
-
-	<!-- login part -->
 	<?php  
-	//no session
-	if(!isset($_SESSION['logged_user'])){
-
-		//no email, no password
-		if(!isset($_POST['email']) && !isset($_POST['password']) ){
+		include 'menu.php'
 	?>
 
-			<div class="user_login">
-
-				<div class="left_most">
-					<p>Cornell Sports Club</p>
-				</div>
-
-				<div class="left_login">
-					<form action="register.php" method="post">
-				        Email: <input type="text" name="email"> <br>
-				        Password: <input type="password" name="password"> <br>
-				        <input type="submit" value="Login" name="submit_login">
-		        	</form>	
-				</div>
-				
-				<div class="right_login">
-					<form action="register.php" method="post">
-			        	<input type="submit" value="Forget password" name="submit_forget_password">
-			        </form>
-
-			        <form action="register.php" method="post">
-			        	<input type="submit" value="Register a new user!" name="submit_register">
-			        </form>	
-				</div>
-		        
-			</div>
-
-	<?php 
-		}
-
-		//submit password and email 
-		if(isset($_POST['email']) && isset($_POST['password']) ){
-			//get email and password
-			$email = (isset($_POST['email']) ? $_POST['email']: null);
-	        $password = (isset($_POST['password']) ? $_POST['password']: null);
-	    	//hash the password
-	        $hashed_password = hash("sha256", $password);
-
-	        //login successfully
-	        $bool = login($email, $password, $mysqli);
-			if($bool == true){
-				header('Location:index.php');
-
-			//failed to login
-			}else{
-				print("<p>Wrong email or password.</p>");
-			}
-
-		}
-
-		//register
-		if(isset($_POST['submit_register'])){
-			header('Location:register.php');
-		}
-
-		//forgot password
-		if(isset($_POST['submit_forget_password'])){
-			header('Location:forget_password.php');
-		}
-
-
-	//find session 
-	}else{
-		$find_username = $_SESSION['logged_user'];
-		//echo "<p>$find_username</p>";
-	?>
-
-		<div class="user_login">
-	<?php  
-		print("<p>Welcome  " .$find_username . "</p>");
-	?>
-			
-			<form action="forget_password.php" method="post">
-				<input type="submit" value="Logout" name="submit_logout">
-			</form>
-		</div>
-
-
-	<?php  
-	}
-	if (isset($_POST['submit_logout'])) {
-		logout();
-	}
-	?>
-
-
-
-
-	<div class="nav">
-	    <ul>
-	      	<li><a href="index.php">ABOUT US</a></li>
-		    <li><a href="teams.php">TEAMS</a></li>
-		    <li><a href="schedule.php">EVENTS</a></li>
-		    <li><a href="news.php">CONTACT</a></li>
-	    </ul>
-	</div>
-
-
-	<div class="container">
+	<div class="forget_password">
 		<form action="forget_password.php" method="post">
 			UserName:<input type="text" name="check_username"> <br>
 	  		Email:<input type="text" name="email_forget"> <br>
@@ -135,7 +32,7 @@
 		$query = "SELECT * FROM user WHERE email='$email_forget'";
 		$result = $mysqli -> query($query);
 		$count = mysqli_num_rows($result);
-		// If the count is equal to one, we will send message other wise display an error message.
+		// If the count is equal to one, we will send message otherwise display an error message.
 		if($count == 1){
 			$rows = mysqli_fetch_array($result);
 
@@ -162,7 +59,9 @@
 
 				$sentmail = mail ( $to, $subject, $body, $headers1 );
 
-				$new_query = "UPDATE user SET password = $pass WHERE email = $to";
+				$new_pass = hash('sha256', $pass);
+
+				$new_query = "UPDATE user SET password = '$new_pass' WHERE email = '$to'";
 				$mysqli -> query($new_query);
 
 				echo "<p>  </p>";
@@ -175,10 +74,12 @@
 
 			// } 
 			else {
-				echo "<p>username and email don't match.</p>";
+				echo "<p style=text-align:center>username and email don't match.</p>";
 			}
 
-
+		}
+		else{
+				echo "<p style=text-align:center>Can't find information for this account</p>";
 		}
 
 		// //If the message is sent successfully, display sucess message otherwise display an error message.
@@ -195,7 +96,9 @@
   	?>
 
 
-
+  	<?php  
+		include 'footer.php'
+	?>
 
 
 </body>
