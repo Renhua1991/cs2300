a0bfba913f07ef89b5efcd423cae7e28dfcaf2dc
Matthew Laks <matthewlaks@dhcp-gs-2268.redrover.cornell.edu>
2015-05-12 00:12:22 -0400

    update

--- footer.php
+++ footer.php
@@ -2,7 +2,5 @@
 <link href='css/headercss.css' rel='stylesheet' type='text/css'>
 <footer>
     Renhua Wang, Matthew Laks and Mengtian Zhao &#169; 2015
-    <div id="fb">
-    <a href="https://www.facebook.com/CornellCSC?fref=ts"><img src="http://sportski-podovi.com/images/uploads/facebook.png" alt="Facebook" title="Facebook"></a>
-    </div>
+    <a id="fb" href="https://www.facebook.com/CornellCSC?fref=ts"><img src="http://sportski-podovi.com/images/uploads/facebook.png" alt="Facebook" title="Facebook"></a>
 </footer>
