791ed70a81e6ea07d5d751ab60c8a4b73af9714c
Matthew Laks <matthewlaks@dhcp-gs-2268.redrover.cornell.edu>
2015-05-12 00:06:50 -0400

    fb logo update

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
 <footer>
     Renhua Wang, Matthew Laks and Mengtian Zhao &#169; 2015
-    <a href="https://www.facebook.com/CornellCSC?fref=ts"><img src="http://assets.sdes.ucf.edu/images/icons/facebook.png" alt="Facebook" title="Facebook"></a>
+    <a href="https://www.facebook.com/CornellCSC?fref=ts"><img src="http://sportski-podovi.com/images/uploads/facebook.png" alt="Facebook" title="Facebook"></a>
 </footer>
